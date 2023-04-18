8a41ecb652f30339c03f06cec7fce99174d0c927
Meer <128205033+JeonHaLin@users.noreply.github.com>
2023-04-18 21:21:57 +0900

    Update Song.java

--- Song.java
+++ Song.java
@@ -1,21 +1,25 @@
-public class Song {
+class Song {
     String title;
     String artist;
     int year;
     String country;
 
-    Song(){}
-    Song(int year, String country, String artist, String title){
+    Song() {
+    }
+
+    Song(int year, String country, String artist, String title) {
         this.year = year;
         this.country = country;
         this.artist = artist;
         this.title = title;
     }
-    public void show(){
+
+    public void show() {
         System.out.println(year + "년 " + country + "국적의 "
                 + artist + "가 부른 " + title);
     }
-
+}
+public class PrintSong{
     public static void main(String[] args){
         Song mySong = new Song(1978, "스웨덴", "ABBA", "Dancing Queen");
         mySong.show();
