12cd78fa375845f5ff016ee5f8beeb9f23b7ae87
Meer <128205033+JeonHaLin@users.noreply.github.com>
2023-04-18 21:23:10 +0900

    Update and rename Grade.java to AvgGrade.java

--- AvgGrade.java
+++ AvgGrade.java
@@ -1,35 +1,44 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Grade {
+class Grade {
 
     private int math, science, english;
 
-    public Grade(){}
+    public Grade() {}
 
-    public Grade(int math, int science, int english){
+    public Grade(int math, int science, int english) {
         this.math = math;
         this.science = science;
         this.english = english;
     }
 
-    public int getGrade(){
+    public int getGrade() {
         Scanner scan = new Scanner(System.in);
-        while(true) {
-            int theGrade = scan.nextInt();
-            if (theGrade < 0) {
-                System.out.println("0 이상의 정수만 입력 가능합니다.");
+        while (true) {
+            try {
+                int theGrade = scan.nextInt();
+                if (theGrade < 0) {
+                    System.out.println("0 이상의 정수만 입력 가능합니다.");
+                    System.out.print("해당 과목의 점수를 다시 입력하세요>> ");
+                } else {
+                    return theGrade;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("정수만 입력 가능한 프로그램입니다.");
                 System.out.print("해당 과목의 점수를 다시 입력하세요>> ");
-                continue;
-            } else {
-                return theGrade;
+                scan.next();
             }
         }
     }
-    public int average(){
+
+    public int average() {
         return (math + science + english) / 3;
     }
 
+}
+
+public class AvgGrade{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
 
@@ -39,9 +48,10 @@ public class Grade {
             int math = scanGrade.getGrade();
             int science = scanGrade.getGrade();
             int english = scanGrade.getGrade();
+            //
 
-            Grade me = new Grade (math, science, english);
-            System.out.println("평균은 " + me.average());
+            Grade myGrade = new Grade (math, science, english);
+            System.out.println("평균은 " + myGrade.average());
 
             scan.close();
         }
