ede7de051ecae91325b456319e9e20f7b3533b2e
Meer <128205033+JeonHaLin@users.noreply.github.com>
2023-04-19 12:54:28 +0900

    Update PhoneBook.java

--- PhoneBook.java
+++ PhoneBook.java
@@ -1,6 +1,18 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+class OperatingPhoneBook {
+    public int inputNum() {
+        while (true) {
+            Scanner scan = new Scanner(System.in);
+            System.out.print("인원수>>");
+            int num = scan.nextInt();
+            if (num <= 0) {
+                System.out.println("0 이하의 정수는 입력할 수 없습니다.");
+            } else return num;
+        }
+    }
+}
 class Phone{
     String name, tel;
 
@@ -14,13 +26,9 @@ public class PhoneBook {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         try {
-            System.out.print("인원수>>");
-            int num = scan.nextInt();
-            if (num <= 0){
-                System.out.println("잘못된 접근입니다.");
-                System.out.println("프로그램을 종료합니다.");
-                System.exit(0);
-            }
+            OperatingPhoneBook op = new OperatingPhoneBook();
+            int num = op.inputNum();
+
             Phone[] myPhone = new Phone[num];
             for (int i = 0; i < num; i++) {
                 System.out.print("이름과 전화번호(이름과 번호는 빈 칸없이 입력)>>");
@@ -59,7 +67,7 @@ public class PhoneBook {
     }
 }
 /*
-1. 인원수를 0 이하의 음수로 입력했을 때
-2.
-3.
+1. 인원수에 양의 정수만 입력
+2. 인원수에 0을 입력하면 프로그램 종료
+3. 문자열이 아닌 데이터 입력
  */
