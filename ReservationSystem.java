e52991edf7344756e6cebef94a845e8ae6e3ddee
Meer <128205033+JeonHaLin@users.noreply.github.com>
2023-04-19 12:28:57 +0900

    Update ReservationSystem.java

--- ReservationSystem.java
+++ ReservationSystem.java
@@ -13,6 +13,36 @@ class OperateSystem{
             }
         }
     }
+    public int inputSeatNum(int seatType){
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("번호>>");
+            int seatNum = scan.nextInt();
+            if(seatNum < 1 || seatNum > 10){
+                System.out.println("잘못된 접근입니다. 다시 입력하세요.");
+                continue;
+            }
+            if (alreadyReserved(seatType, seatNum)){
+                return 0;
+            }
+
+            else return seatNum;
+        }
+    }
+
+    public int inputSeatType(){
+        while(true){
+            Scanner scan = new Scanner(System.in);
+            System.out.print("좌석구분 S(1), A(2), B(3)>>");
+            int seatType = scan.nextInt();
+
+            if (seatType<1||seatType>3){
+                System.out.println("잘못된 접근입니다. 다시 입력하세요.");
+            }
+            else return seatType;
+        }
+    }
     public void addPerson(int seatType, String name, int seatNum){
         arr[seatType-1][seatNum] = name;
     }
@@ -40,6 +70,35 @@ class OperateSystem{
         }
         System.out.println("<<<조회를 완료하였습니다>>>");
     }
+
+    public boolean isEmpty(int seatType){
+        int temp = 0;
+        for (int i = 1; i < 11; i++){
+            if (arr[seatType-1][i].equals("---"))
+                temp++;
+        }
+        return temp == 10;
+    }
+
+    public boolean isFull(int seatType){
+        int temp = 0;
+        for (int i = 1; i < 11; i++){
+            if (arr[seatType-1][i].equals("---"))
+                temp++;
+        }
+        return temp == 0;
+    }
+    public boolean isThere(int seatType, String name){
+        int temp = 0;
+        for (int i = 1; i < 11; i++){
+            if (arr[seatType-1][i].equals(name))
+                temp++;
+        }
+        return temp != 0;
+    }
+    public boolean alreadyReserved(int seatType, int seatNum){
+        return !arr[seatType-1][seatNum].equals("---");
+    }
 }
 
 public class ReservationSystem {
@@ -57,15 +116,23 @@ public class ReservationSystem {
 
                 switch (menu){
                     case 1:{
-                        System.out.print("좌석구분 S(1), A(2), B(3)>>");
-                        int seatType = scan.nextInt();
+                        int seatType = os.inputSeatType();
                         os.showSeats(seatType);
 
                         System.out.print("이름>>");
                         String name = scan.next();
+                        int seatNum = os.inputSeatNum(seatType);
+                        if (seatNum == 0){
+                            System.out.println("이미 매진된 좌석입니다.");
+                            System.out.println("메인 메뉴로 돌아갑니다.");
+                            break;
+                        }
+                        if (os.isFull(seatType)){
+                            System.out.println("전 석 매진입니다.");
+                            System.out.println("메인메뉴로 돌아갑니다.");
+                            break;
+                        }
 
-                        System.out.print("번호>>");
-                        int seatNum = scan.nextInt();
                         os.addPerson(seatType, name, seatNum);
                         break;
                     }
@@ -74,21 +141,28 @@ public class ReservationSystem {
                         break;
                     }
                     case 3:{
-                        System.out.print("좌석 S:1, A:2, B:3>>");
-                        int seatType = scan.nextInt();
+                        int seatType = os.inputSeatType();
                         os.showSeats(seatType);
+                        if (os.isEmpty(seatType)){
+                            System.out.println("취소할 수 있는 좌석이 없습니다.");
+                            System.out.println("메인메뉴로 돌아갑니다.");
+                            break;
+                        }
 
                         System.out.print("이름>>");
                         String name = scan.next();
+                        if(!os.isThere(seatType, name)){
+                            System.out.println("해당 이름이 없습니다.");
+                            System.out.println("메인 메뉴로 돌아갑니다.");
+                        }
                         os.delPerson(name, seatType);
                         break;
                     }
                     case 4:
                         System.exit(0);
                     default:{
-                        System.out.println("잘못된 접근입니다.");
-                        System.out.println("시스템을 종료합니다.");
-                        System.exit(0);
+                        System.out.println("잘못된 접근입니다. 다시 입력하세요.");
+                        break;
                     }
                 }
             }
@@ -105,8 +179,11 @@ public class ReservationSystem {
 }
 /*
 예외처리만 하면 됨!!!!
-1. 메인 메뉴에서 1~4 아닌 숫자 입력할 때
-2. 좌석 타입에서 1~3 아닌 숫자 입력할 때
-3. 번호에서 1~10 아닌 숫자 입력할 때
+v 1. 메인 메뉴에서 1~4 아닌 숫자 입력할 때 >> 잘못된 접근입니다.
+v 2. 좌석 타입에서 1~3 아닌 숫자 입력할 때 >> 잘못된 접근입니다.
+v 3. 번호에서 1~10 아닌 숫자 입력할 때 >> 잘못된 접근입니다.
+v 4. 빈 열에 뭔가를 취소하려고 할때 >> 좌석이 모두 비어있습니다.
+v 5. 이름이 없을 때>> 예약 내역이 없습니다.
+v 6. 열에 모든 좌석이 예약되어 있는데 더 예약하려 할 때 >> 매진입니다.
+종료하면 안되고 retry 하도록 해야함!!!!!!!!!!!
  */
-
